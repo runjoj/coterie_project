9c6629dcafc23af68e571e28423fba69787aee30
jodillion <dillionjoann@gmail.com>
2019-12-08 09:45:07 -0500

    updated api v1 profiles container to add index

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -3,6 +3,11 @@ class Api::V1::LocationsController < ApplicationController
 
   before_action :authenticate_user
 
+  def index
+    profiles = Profile.all
+    render json: profiles
+  end
+
   def show
     profile = Profile.find(params["id"])
     render json: profile
