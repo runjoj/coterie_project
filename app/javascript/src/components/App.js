31f78040622c2b6654b4cf87ac0f750707796937
jodillion <dillionjoann@gmail.com>
2019-12-07 23:06:10 -0500

    update App with routes and switch

--- app/javascript/src/components/App.js
+++ app/javascript/src/components/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Router, Switch } from "react-router-dom"
 
-import PageContainer from '../containers/PageContainer'
+import ProfileContainer from '../containers/ProfileContainer'
+import ShowProfileContainer from '../containers/ShowProfileContainer'
 
 const App = (props) => {
   return (
     <Provider store={props.store}>
-      <PageContainer />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/profiles" component={ProfileContainer} />
+          <Route exact path="/profiles/:id" component={ShowProfileContainer} />
+        </Switch>
+      </BrowserRouter>
     </Provider>
   )
 }
