e6b127967bfbc8623113c0829009e34e50020776
jodillion <dillionjoann@gmail.com>
2019-12-08 09:49:46 -0500

    update profiles reducer and rootReducer to fix errors

--- app/javascript/src/store/rootReducer.js
+++ app/javascript/src/store/rootReducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 
 import { alertMessage } from '../modules/alertMessage'
 import { summaries } from '../modules/summaries'
+import { profiles } from '../modules/profiles'
 
 let rootReducer = combineReducers({
   summaries,
