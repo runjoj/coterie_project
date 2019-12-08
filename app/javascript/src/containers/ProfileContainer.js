b7c6622ded3678f7c9007458dbc48412f1656f65
jodillion <dillionjoann@gmail.com>
2019-12-08 12:49:41 -0500

    filled out Profile Container and connected to profiles reducer

--- app/javascript/src/containers/ProfileContainer.js
+++ app/javascript/src/containers/ProfileContainer.js
@@ -7,13 +7,45 @@ class ProfileContainer extends Component {
     super(props)
   }
 
+  componentDidMount() {
+    this.props.getProfiles()
+  }
+
   render() {
+    let profiles = this.props.allProfiles.map((profile) => {
+      let name = profile.name
+
+      return (
+        <Profile
+          key={id}
+          name={name}
+        />
+      )
+    })
     return (
       <div>
+        <ul>
+          {profiles}
+        </ul>
         <ProfileFormContainer />
       </div>
     )
   }
 }
 
-export default ProfileContainer
+const mapStateToProps = (state) => {
+  return {
+    allProfiles: state.profiles.allProfiles
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    allProfiles: () => dispatch(getProfiles())
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ProfileContainer)
