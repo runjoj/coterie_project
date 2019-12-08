cef767477e798f86b2b34a4d02cf01eb99a99001
jodillion <dillionjoann@gmail.com>
2019-12-08 09:45:18 -0500

    created Profile Container to hold form

---
+++ app/javascript/src/containers/ProfileContainer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+import ProfileFormContainer from './ProfileFormContainer'
+
+class ProfileContainer extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <ProfileFormContainer />
+      </div>
+    )
+  }
+}
+
+export default ProfileContainer
